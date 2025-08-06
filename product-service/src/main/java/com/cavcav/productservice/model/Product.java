e5476667f1158f9971292909bf2e83dbd7728c0b
Ömer Sarıtoy <101520451+omersaritoy@users.noreply.github.com>
2025-08-06 21:38:29 +0300

    added gateway service

--- product-service/src/main/java/com/cavcav/productservice/model/Product.java
+++ product-service/src/main/java/com/cavcav/productservice/model/Product.java
@@ -2,11 +2,17 @@ package com.cavcav.productservice.model;
 
 
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
 @Document(value = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
@@ -15,48 +21,5 @@ public class Product {
     private String description;
     private BigDecimal price;
 
-    public Product(String id, String name, String description, BigDecimal price) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-    }
 
-    public Product(String name, String description, BigDecimal price) {
-        this.name = name;
-        this.description = description;
-        this.price = price;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
 }
